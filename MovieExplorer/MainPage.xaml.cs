4d2a4614da14e1ba0c6835b2f8259df43b8141ec
Brian Harper <bgharper@yahoo.com>
2018-07-10 13:15:03 -0400

    Initial commit

---
+++ MovieExplorer/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MovieExplorer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            RootObject myMovie =
+                await OmdbData.GetMovie(50.1, 20.2);
+            var myPoster = myMovie.Poster;
+
+            ResultImage.Source = new BitmapImage(new Uri(myPoster, UriKind.Absolute));
+            ResultTextBlock.Text = myMovie.Title + " - " + myMovie.Year;
+            PosterURLTextBlock.Text = myPoster;
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+            var SearchTarget = Title.Text;
+
+            ///OmdbSearch.SearchRootObject myMovie = await OmdbSearch.SearchMovie(50.1, 20.2);
+            OmdbSearch.SearchRootObject myMovie = await OmdbSearch.SearchMovie(SearchTarget);
+
+            ///string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+            ///ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+            SearchResultTB.Text = "Response: " + myMovie.Response + " " + myMovie.totalResults;
+            ///SearchResultTB.Text = string(myMovie);
+            ///var myPoster = myMovie.Poster;
+            ///PosterURLTextBlock.Text = myPoster;
+            //SearchResultTB.Text = "Hello";
+
+
+        }
+
+    }
+
+}
---
+++ MovieExplorer/omdbsearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExplorer
+{
+    class OmdbSearch
+    {
+        public async static Task<SearchRootObject> SearchMovie(string search)
+        {
+            var http = new HttpClient();
+
+            var url = String.Format("http://www.omdbapi.com/?apikey=ff21610b&s={0}", search);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(SearchRootObject));
+
+            var ms = new System.IO.MemoryStream(Encoding.UTF8.GetBytes(result));
+
+            var data  = (SearchRootObject)serializer.ReadObject(ms);
+
+            ///ms.Position = 0;
+            ///SearchRootObject first = (SearchRootObject)serializer.ReadObject(ms);
+            return data;
+        }
+
+        [DataContract]
+        public class Search
+        {
+            [DataMember]
+            public string Title { get; set; }
+
+            [DataMember]
+            public string Year { get; set; }
+
+            [DataMember]
+            public string imdbID { get; set; }
+
+            [DataMember]
+            public string Type { get; set; }
+
+            [DataMember]
+            public string Poster { get; set; }
+        }
+
+        [DataContract]
+        public class SearchRootObject
+        {
+            [DataMember]
+            public List<Search> Search { get; set; }
+
+            [DataMember]
+            public string totalResults { get; set; }
+
+            [DataMember]
+            public string Response { get; set; }
+        }
+    }
+}
